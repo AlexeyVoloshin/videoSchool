64c4ea3bb7473ad19ec607851e24379f0fb94ca1
Alexey Voloshin <voloshinalex1100@gmail.com>
2019-01-27 23:14:30 +0200

    add bootstrap navbar

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,9 +1,29 @@
+<?php
+    use \yii\bootstrap\NavBar;
+?>
+<?php $this->beginPage(); ?>
 <html>
     <head>
         <title>VideoSchool</title>
+        <?php $this->head(); ?>
     </head>
     <body>
+    <?php $this->beginBody() ?> //подключили возможность размещения скриптов jquery
+        <?php
+        //передаем параметры в готовую верстку bootstrap-a. получился навбар
+            NavBar::begin([
+                    'brandLabel' => 'VideoSchool',
+                'brandUrl' => Yii::$app->homeUrl,
+                'options' => [
+                        'class' => 'navbar-default navbar-fixed-top'
+                ]
+            ]);
+            NavBar::end();
+        ?>
+    <div class="container" style="margin-top: 50px">
         <?= $content ?>
-
+    </div>
+    <?php $this->endBody(); ?>
     </body>
 </html>
+<?php $this->endPage(); ?>
