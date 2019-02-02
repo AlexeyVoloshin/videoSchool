b484b965ef03b326b2241fe6f109baf6125ac425
voloshinalex <voloshinalex1100@gmail.com>
2019-02-02 10:34:05 +0200

    add validation form user login

--- models/UserJoinForm.php
+++ models/UserJoinForm.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\models;
+use http\Message;
 use yii\base\Model;
 
 class UserJoinForm extends Model
@@ -9,4 +10,20 @@ class UserJoinForm extends Model
         public $email;
         public $password;
         public $password2;
+
+        public function rules() //выполняет валидацию формы
+        {
+
+            return [
+
+                ['name', 'required', 'message' => 'Имя пользователя не может быть пустым'],//проверяет поля ввода на наличие введеных данных
+                ['email', 'required'],
+                ['password', 'required'],
+                ['password2', 'required'],
+                ['name', 'string', 'min' =>3, 'max' =>30, 'message' => 'имя сильно короткое'],//параметр max, min задает диапазон длинны пароля
+                ['email', 'email', 'message' => 'Адрес эл. почты указан не верно'], //параметр massage помогает изменить стандартное сообщение
+                ['password', 'string', 'min' => 4],
+                ['password2', 'compare', 'compareAttribute' => 'password']//правило сравнения compare, которое содержит атрибут сравнивания compareAttribute
+            ];
+        }
     }
